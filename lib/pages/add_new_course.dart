39c4f00029fb125cb2372845cb13acd963ae1362
ThilinaMadhusanka <140851032+ThilinaMadhusanka@users.noreply.github.com>
2025-03-06 10:40:16 -0800

    update add new page

--- lib/pages/add_new_course.dart
+++ lib/pages/add_new_course.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:study_planner_new/constants/colors.dart';
+import 'package:study_planner_new/widgets/custum_input.dart';
 
 class AddNewCourse extends StatelessWidget {
   AddNewCourse({super.key});
@@ -37,6 +39,67 @@ class AddNewCourse extends StatelessWidget {
                 ),
                 SizedBox(
                   height: 5,
+                ),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Quisque vehicula, erat in facilisis viverra, libero justo fermentum purus",
+                  style: TextStyle(
+                    fontSize: 13,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustumInput(
+                  lableText: "Course name",
+                  controller: TextEditingController(),
+                  //validator: (p0) {},
+                  //key: GlobalKey(),
+                ),
+                CustumInput(
+                  lableText: "Description",
+                  controller: TextEditingController(),
+                  //validator: (p0) {},
+                  //key: GlobalKey(),
+                ),
+                CustumInput(
+                  lableText: "Duration",
+                  controller: TextEditingController(),
+                  //validator: (p0) {},
+                  //key: GlobalKey(),
+                ),
+                CustumInput(
+                  lableText: "Shedule",
+                  controller: TextEditingController(),
+                  //validator: (p0) {},
+                  //key: GlobalKey(),
+                ),
+                CustumInput(
+                  lableText: "Instructor",
+                  controller: TextEditingController(),
+                  //validator: (p0) {},
+                  //key: GlobalKey(),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 12,
+                    ),
+                  ),
+                  child: Text(
+                    "Add Course",
+                    style: TextStyle(
+                      backgroundColor: primaryColor,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ],
             ),
