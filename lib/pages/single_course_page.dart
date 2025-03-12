16cb9aa5a8777aa0da2d641148ea13c733925b6d
ThilinaMadhusanka <140851032+ThilinaMadhusanka@users.noreply.github.com>
2025-03-12 10:42:31 -0700

    Update

--- lib/pages/single_course_page.dart
+++ lib/pages/single_course_page.dart
@@ -17,6 +17,7 @@ class SingleCoursePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Course details
             Text(
               course.name,
               style: TextStyle(
