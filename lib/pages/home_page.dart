7a665d0911b1a9a3744b33351e7c3c3331f5e61f
ThilinaMadhusanka <140851032+ThilinaMadhusanka@users.noreply.github.com>
2025-03-12 10:48:36 -0700

    Update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -27,6 +27,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Add Bottom navigation bar
       bottomNavigationBar: BottomNavigationBar(
         onTap: _onTapItem,
         currentIndex: _selectedIndex,
